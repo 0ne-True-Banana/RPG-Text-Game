69114062d08431cf7fd2a69733d4606cc4f3a43b
Jaden A <jadenananaba2010@gmail.com>
2024-09-25 18:27:22 +0100

    Add files via upload
    A python project aiming to imitate the design of an RPG game all the while using only text to provide a new experience

---
+++ Rpg game.py
@@ -0,0 +1,76 @@
+import time
+import sys
+
+print ("The Land Amongst the Stars")
+def begin():
+    input ("Type 'Y' to start or 'N' to exit ")
+    if input == "Y":
+        print ("A long time ago, there was no world")
+        print ("A long time ago, the sky was clean")
+        print ("A long time ago, a city existed")
+        print ("One which laid amongst the many stars")
+        print ("It was called The Nebulus")
+        print ("A land, high above the clouds, where different cosmic beings reined")
+        print ("The four races, the Angels, The Solonix, The Mechans, and The Lumni")
+        print ("All of the races once lived in peace")
+        print ("They all worked together in perfect harmony")
+        print ("If only they didn't begin to question....")
+        print("*sigh* Soon, the mechans, in their usual fashion, began to question the order of things.")
+        print ("They believed that their world should be run by purely by one ruler, and not be divided")
+        print ("Of course, the other factions didn't believe in this notion")
+        print ("The, the world of the sky was thrown into chaos")
+        print ("Some began to move to the land on the ground.")
+        print ("Soon, the land in the sky was empty.")
+        print ("Then, HE, was awakened")
+        print ("He simply wished to devour, more and more,")
+        print ("Till the world simply ended")
+        print ("Only HE, was left")
+        print ("Then, ugh, I, had to intervene")
+        print ("I banished him, to the deepest pits of the universe, never to be seen again")
+        print ("I regenerated the world. Then, it was at peace")
+        print ("I decided to rest. And leave his next awakening, to the next generation")
+        print ("That is where you come in")
+    elif input == "N":
+        print ("Well, why did you start the game then?")
+        print ("Anyways, bye.")
+        sys.exit(0)
+begin()
+
+name = input ("But first, what shall be your name?")
+print ("Wonderful choice, " +name, "A really wonderful name")
+print ("Now, awaken")
+print ("You wake up, in a room, a dark room.")
+health = 20
+spirit = 15
+armour = 0
+inventory = {}
+print ("You look to your left, but no-one is there")
+print ("You look to your right, seeing a dark figure, looming in a chair")
+print ("Τζέρεμι: Hello")
+
+def Jeremy_response():
+    print ("What shall you say to him:")
+    print ("a. Hello,")
+    print ("b. Where am I?")
+    response1 = input ( )
+    if response1 == "a":
+        print ("Ah, you're awake")
+        print ("We have a lot to discuss")
+    elif response1 == "b":
+        print ("Oh?, Not even polite enough to say hello?")
+        print ("Anyways, we have a lot to discuss")
+
+Jeremy_response()
+print ("You are special, you are not like others")
+print ("Glad you still know how to speak.")
+print ("You're gonna be doing a lot of that....")
+print ("Alright, go the bathroom and get your armour")
+
+#s
+
+
+
+
+
+
+    
